f7f06029a1223a1874ad2f7326eb5569b52069fe
Ben Adams <thundercat@illyriad.co.uk>
2018-09-02 12:24:45 +0100

    Don't capture asynclocals to Cleanup timer

--- src/Microsoft.Extensions.Http/DefaultHttpClientFactory.cs
+++ src/Microsoft.Extensions.Http/DefaultHttpClientFactory.cs
@@ -241,7 +241,26 @@ namespace Microsoft.Extensions.Http
             {
                 if (_cleanupTimer == null)
                 {
-                    _cleanupTimer = new Timer(_cleanupCallback, this, DefaultCleanupInterval, Timeout.InfiniteTimeSpan);
+                    // Don't capture the current ExecutionContext and its AsyncLocals onto the timer
+                    bool restoreFlow = false;
+                    try
+                    {
+                        if (!ExecutionContext.IsFlowSuppressed())
+                        {
+                            ExecutionContext.SuppressFlow();
+                            restoreFlow = true;
+                        }
+
+                        _cleanupTimer = new Timer(_cleanupCallback, this, DefaultCleanupInterval, Timeout.InfiniteTimeSpan);
+                    }
+                    finally
+                    {
+                        // Restore the current ExecutionContext
+                        if (restoreFlow)
+                        {
+                            ExecutionContext.RestoreFlow();
+                        }
+                    }
                 }
             }
         }
